15c3165fdddcbd6b98377afced988c3e036bfc23
OscarPS11 <OscarPS11@192.168.1.79>
2018-02-26 13:17:02 +0100

    psuh

--- src/Segunda.java
+++ src/Segunda.java
@@ -1,4 +1,12 @@
 
 public class Segunda {
 int entero;
+
+public Segunda(int entero) {
+	super();
+	this.entero = entero;
+	Primera primera= new Primera();
+	System.out.println(primera.oreo);
+}
+
 }
